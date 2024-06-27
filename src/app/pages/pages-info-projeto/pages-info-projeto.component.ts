152cc8cb32918f9b31fb331bf537f394ec39f2d2
Roberio Albuquerque <roberioalbuquerque.neto@gmail.com>
2024-06-27 12:16:09 -0300

    feat: Ná Página de informçaões do projeto foram criados botões de Informações e Membros, para exibilos dinamicamente.

--- src/app/pages/pages-info-projeto/pages-info-projeto.component.ts
+++ src/app/pages/pages-info-projeto/pages-info-projeto.component.ts
@@ -3,7 +3,6 @@ import { ActivatedRoute } from '@angular/router';
 import { LocalStorageProjetosService } from 'src/app/service/local-storage-projetos.service';
 import { Projeto } from 'src/app/interface/projeto';
 
-
 @Component({
   selector: 'app-pages-info-projeto',
   templateUrl: './pages-info-projeto.component.html',
@@ -17,9 +16,30 @@ export class PagesInfoProjetoComponent implements OnInit {
     private localStorageProjetosService: LocalStorageProjetosService
   ) {}
 
+  mostrarMembros = false;
+  mostraInfo = false;
+
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.projeto = this.localStorageProjetosService.getProjectById(id);
-    console.log(this.projeto?.name)
+    console.log(this.projeto?.name);
+  }
+
+  mostraMembros(): void {
+    this.mostrarMembros = !this.mostrarMembros;
+    if (this.mostrarMembros) {
+      this.mostraInfo = false;
+    }
+  }
+
+  mostrarInfo(): void {
+    this.mostraInfo = !this.mostraInfo;
+    if (this.mostraInfo) {
+      this.mostrarMembros = false;
+    }
+  }
+
+  capitalizeFirstLetter(name: string): string {
+    return name.charAt(0).toUpperCase() + name.slice(1);
   }
 }
